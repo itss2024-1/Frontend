eb3bd292740b5ae18db5f16405df32c7a6b956cf
iaht211 <nguyenvanthai092003@gmail.com>
2024-12-28 10:10:10 +0700

    fix login and register

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -65,48 +65,101 @@ const LoginPage = () => {
                 <h3>Đăng nhập</h3>
                 <Form
                     name="basic"
-                    labelCol={{ span: 6 }}
-                    style={{ maxWidth: 600, margin: "0 auto" }}
-                    initialValues={{ remember: true }}
+                    labelCol={{
+                        span: 7,
+                    }}
+                    wrapperCol={{
+                        span: 17,
+                    }}
+                    style={{
+                        maxWidth: '400px',
+                        margin: '0 auto',
+                    }}
+                    initialValues={{
+                        remember: true,
+                    }}
                     onFinish={onFinish}
                     onFinishFailed={onFinishFailed}
                     autoComplete="off"
                 >
+                    {/* Email */}
                     <Form.Item
-                        label="Email"
+                        label="Email:"
                         name="username"
-                        rules={[{ required: true, message: 'Please input your email!' }]}
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your email!',
+                            },
+                        ]}
                     >
-                        <Input />
+                        <Input style={{ height: '35px', borderRadius: '5px' }} />
                     </Form.Item>
-
+                    {/* Password */}
                     <Form.Item
-                        label="Password"
+                        label="Password:"
                         name="password"
-                        rules={[{ required: true, message: 'Please input your password!' }]}
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your password!',
+                            },
+                        ]}
+                        style={{ marginBottom: '20px' }}
                     >
-                        <Input.Password />
+                        <Input.Password style={{ height: '35px', borderRadius: '5px' }} />
                     </Form.Item>
-
+                    {/* Remember Me */}
                     <Form.Item
                         name="remember"
                         valuePropName="checked"
-                        wrapperCol={{ offset: 8, span: 16 }}
+                        wrapperCol={{
+                            offset: 7,
+                            span: 17,
+                        }}
+                        style={{ marginBottom: '20px' }}
                     >
                         <Checkbox>Remember me</Checkbox>
                     </Form.Item>
-
+                    {/* Login Button */}
                     <Form.Item
-                        wrapperCol={{ offset: 8, span: 16 }}
+                        wrapperCol={{
+                            span: 24,
+                        }}
+                        style={{ textAlign: 'center', marginBottom: '10px' }}
                     >
-                        <Button type="primary" htmlType="submit" loading={isSubmit}>
+                        <Button
+                            type="primary"
+                            htmlType="submit"
+                            loading={isSubmit}
+                            style={{
+                                width: '150px',
+                                borderRadius: '5px',
+                                marginBottom: '10px',
+                                backgroundColor: '#FF4D4F',
+                                borderColor: '#FF4D4F',
+                                color: 'white',
+                            }}
+                        >
                             Đăng nhập
                         </Button>
                     </Form.Item>
+                    {/* Register Link */}
+                    <div style={{ textAlign: 'center', marginTop: '10px' }}>
+                        <span style={{ color: '#888' }}>Bạn chưa có tài khoản? </span>
+                        <span
+                            onClick={() => handleChange()}
+                            style={{
+                                color: '#FF4D4F',
+                                cursor: 'pointer',
+                                textDecoration: 'underline',
+                                fontWeight: '500',
+                            }}
+                        >
+                            Đăng ký ngay
+                        </span>
+                    </div>
                 </Form>
-                <Button type="link" onClick={() => handleChange()}>
-                    Chuyển sang Đăng ký
-                </Button>
             </>
         )
     }
@@ -116,56 +169,164 @@ const LoginPage = () => {
             <>
                 <h3>Đăng ký</h3>
                 <Form
-                    name="basic"
-                    labelCol={{ span: 6 }}
-                    style={{ maxWidth: 600 }}
-                    initialValues={{ remember: true }}
+                    name="register"
+                    labelCol={{
+                        span: 10,
+                    }}
+                    wrapperCol={{
+                        span: 17,
+                    }}
+                    style={{
+                        maxWidth: '400px',
+                        margin: '0 auto',
+                    }}
                     onFinish={onFinish}
                     onFinishFailed={onFinishFailed}
                     autoComplete="off"
                 >
                     <Form.Item
-                        label="Email"
-                        name="email"
-                        rules={[{ required: true, message: 'Please input your email!' }]}
+                        label="Tên:"
+                        name="name"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Vui lòng nhập tên!',
+                            },
+                        ]}
+                        style={{
+                            marginBottom: '15px',
+                        }}
                     >
                         <Input />
                     </Form.Item>
-
                     <Form.Item
-                        label="Password"
+                        label="Email:"
+                        name="email"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Vui lòng nhập email!',
+                            },
+                        ]}
+                        style={{
+                            marginBottom: '15px',
+                        }}
+                    >
+                        <Input />
+                    </Form.Item>
+                    <Form.Item
+                        label="Số điện thoại:"
+                        name="phone"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Vui lòng nhập số điện thoại!',
+                            },
+                        ]}
+                        style={{
+                            marginBottom: '15px',
+                        }}
+                    >
+                        <Input />
+                    </Form.Item>
+                    <Form.Item
+                        label="Mật khẩu:"
                         name="password"
-                        rules={[{ required: true, message: 'Please input your password!' }]}
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Vui lòng nhập mật khẩu!',
+                            },
+                        ]}
+                        style={{
+                            marginBottom: '15px',
+                        }}
                     >
                         <Input.Password />
                     </Form.Item>
-
                     <Form.Item
-                        label="Tên hiển thị"
-                        name="name"
-                        rules={[{ required: true, message: 'Name không được để trống!' }]}
+                        label="Xác nhận mật khẩu:"
+                        name="confirm"
+                        dependencies={['password']}
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Vui lòng xác nhận mật khẩu!',
+                            },
+                            ({ getFieldValue }) => ({
+                                validator(_, value) {
+                                    if (!value || getFieldValue('password') === value) {
+                                        return Promise.resolve();
+                                    }
+                                    return Promise.reject(new Error('Mật khẩu không khớp!'));
+                                },
+                            }),
+                        ]}
+                        style={{
+                            marginBottom: '20px',
+                        }}
                     >
-                        <Input />
+                        <Input.Password />
                     </Form.Item>
-                    <Form.Item
-                        label="Số điện	thoại"
-                        name="phone"
-                        rules={[{ required: true, message: 'Phone không được để trống!' }]}
+                    {/* Điều khoản */}
+                    <div
+                        style={{
+                            textAlign: 'center',
+                            marginBottom: '20px',
+                            fontSize: '13px',
+                            color: '#888',
+                        }}
                     >
-                        <Input />
-                    </Form.Item>
-
+                        Bằng việc đăng ký, bạn đã đồng ý với{' '}
+                        <span
+                            style={{
+                                color: '#FF4D4F',
+                                cursor: 'pointer',
+                                textDecoration: 'underline',
+                            }}
+                            onClick={() => message.info('Điều khoản sử dụng sẽ được mở.')}
+                        >
+                            các điều khoản của chúng tôi
+                        </span>
+                        .
+                    </div>
                     <Form.Item
-                        wrapperCol={{ offset: 8, span: 16 }}
+                        wrapperCol={{
+                            span: 24,
+                        }}
+                        style={{ textAlign: 'center' }}
                     >
-                        <Button type="primary" htmlType="submit" loading={isSubmit}>
+                        <Button
+                            type="primary"
+                            htmlType="submit"
+                            style={{
+                                width: '150px',
+                                borderRadius: '5px',
+                                backgroundColor: '#FF4D4F', // Nút đỏ
+                                borderColor: '#FF4D4F', // Biên đỏ
+                                color: 'white', // Chữ trắng
+                            }}
+                        >
                             Đăng ký
                         </Button>
                     </Form.Item>
-                    <Button type="link" onClick={() => handleChange()}>
-                        Chuyển sang Đăng nhập
-                    </Button>
+                    {/* Register Link */}
+                    <div style={{ textAlign: 'center', marginTop: '10px' }}>
+                        <span style={{ color: '#888' }}>Bạn đã có tài khoản? </span>
+                        <span
+                            onClick={() => handleChange()}
+                            style={{
+                                color: '#FF4D4F',
+                                cursor: 'pointer',
+                                textDecoration: 'underline',
+                                fontWeight: '500',
+                            }}
+                        >
+                            Quay lại đăng nhập
+                        </span>
+                    </div>
                 </Form>
+
             </>
         )
     }
